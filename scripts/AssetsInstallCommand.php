e0d030a395744227a8b9b7dc2b77148f502a50fa
enjoys <deadly.info@gmail.com>
2023-09-01 20:15:00 +0300

    update post-install-cmd script

---
+++ scripts/AssetsInstallCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EnjoysCMS\ContentEditor\AceEditor\Composer\Scripts;
+
+use EnjoysCMS\Core\Console\Command\AbstractAssetsInstallCommand;
+
+class AssetsInstallCommand extends AbstractAssetsInstallCommand
+{
+    protected string $cwd = __DIR__ . '/..';
+}
